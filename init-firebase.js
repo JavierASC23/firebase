9e0076835129fbae82357e6b6078271e2f08650a
Javier <Jvasquez5422@pelhamprepacademy.org>
2023-08-09 14:42:19 -0400

    Completed database work in it's entirety

--- init-firebase.js
+++ init-firebase.js
@@ -3,8 +3,14 @@
  * provided for you by firebase after creating your project!
 */
 const firebaseConfig = {
-    
-};
+    apiKey: "AIzaSyDBrFJiy9n4NGOxtvbOXWhZZe49X78cN3Y",
+    authDomain: "fir-test-9f4f2.firebaseapp.com",
+    projectId: "fir-test-9f4f2",
+    storageBucket: "fir-test-9f4f2.appspot.com",
+    messagingSenderId: "85585939714",
+    appId: "1:85585939714:web:5bfcae4e746549c6b8461b",
+    measurementId: "G-ZGTD7989S2"
+  };
 
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
